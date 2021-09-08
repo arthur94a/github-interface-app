1abf0a11edc6df10cace39961fc6404c6f72141f
arthur94a <arthuraraujo94@hotmail.com>
2021-09-08 19:26:51 -0300

    class 5 | getting data from github

--- src/components/repositories/index.js
+++ src/components/repositories/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import RepositoryItem from '../repository-item';
 import * as S from "./styled";
 
 const Repositories = () => {
@@ -11,8 +12,20 @@ const Repositories = () => {
                 <S.WrapperTab>Repositories</S.WrapperTab>
                 <S.WrapperTab>Starred</S.WrapperTab>
             </S.WrapperTabList>
-            <S.WrapperTabPanel>panel Repositories</S.WrapperTabPanel>
-            <S.WrapperTabPanel>panel Starred</S.WrapperTabPanel>
+            <S.WrapperTabPanel>
+                <RepositoryItem
+                    name="front-page-instagram"
+                    linkToRepo="https://github.com/arthur94a/front-page-instagram"
+                    fullName="arthur94a/front-page-instagram"
+                />
+            </S.WrapperTabPanel>
+            <S.WrapperTabPanel>
+                <RepositoryItem
+                    name="frontendMentor-FAQcard"
+                    linkToRepo="https://github.com/arthur94a/frontendMentor-FAQcard"
+                    fullName="arthur94a/frontendMentor-FAQcard"
+                />
+            </S.WrapperTabPanel>
         </S.WrapperTabs>
     );
 };
